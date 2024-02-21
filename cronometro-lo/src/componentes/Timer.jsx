767a7e3fe92aeb7a0a5b9a7e97875accdd3766c5
Lorrayne <lorraynelimamota@gmail.com>
2024-02-21 10:43:32 -0300

    finish : setInterval starting: clearInterval

--- cronometro-lo/src/componentes/Timer.jsx
+++ cronometro-lo/src/componentes/Timer.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react"
 
 // SETTIMEOUT: É UM MEDTODO QUE EXECUTARÁ UM CODIGO APÓS O TEMPO ESPECIFICADO(DELAY), o tempo correspondido por milisegundos (3s = 3000 ms)
+// SINTAXE: setTimeout(()=>{},tempo)
 export default function Timer(){
 
 const Mensagem = () => {
@@ -20,11 +21,25 @@ const Add = () => {
 // retorno com {} quer  dizer um retorno com função
 
 //SETINTERVAL: É O METODO QUE PERMITE EXECUTAR REPETIDAMENTE UM BLOCO DE CODIGO A CADA INTERVALO DE TEMPO. EXEMPLO: DE 3 EM 3 SEGUNDOS EXECUTE A AÇÃO.
+//SINTAXE setInterval(()=>{o que ele irá fazer}, tempo)
 
 const [timer, setTimer] = useState(0);
 
-const Start = () => {setInterval(() => 
-    {setTimer((prevTimer) => prevTimer + 1), 1000})
+// setInterval(()=>{setTime(prevTime => prevTime + 1)}, 1000)
+
+const Start = () => {
+    const stopInterval = setInterval(() => {setTimer((prevTimer) => prevTimer + 1)}, 1000)
+
+    setPause(stopInterval)
+}
+
+//clearInterval: Interrompe a execução repetida de um bloco, o setIterval.
+//inicia-se indo na função que inicia colocando o interval como const e setando o estado 
+
+const [pause, setPause] = useState ()
+
+const Stop = () => {
+    clearInterval(pause)
 }
 
 
@@ -39,7 +54,7 @@ const Start = () => {setInterval(() =>
                 <h2>Cronômetro</h2>
                 <h2>{timer}</h2>
                 <button onClick={Start}>Play</button>
-                <button>Pause</button>
+                <button onClick={Stop}>Pause</button>
                 <button>Clear</button>
             </div>
         </main>
